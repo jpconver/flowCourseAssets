a519d42c844ea418f02e1ab39daf30384961e389
jpconver <jpconver@gmail.com>
2023-02-21 18:16:10 -0300

    script helper to regenerate videos

---
+++ scripts/generateVideos.sh
@@ -0,0 +1,12 @@
+/home/jpconver/workspaces/browxy_amazon_2/cmpreqs/video/processBrowxyVideo.sh --input emptyFlowRun.mp4 --output emptyFlowRun.gif --fontSize 50 --text "Correr flujo vacío"
+/home/jpconver/workspaces/browxy_amazon_2/cmpreqs/video/processBrowxyVideo.sh --input goalView.mp4 --output goalView.gif --fontSize 50 --text "Descripción Desafío"
+/home/jpconver/workspaces/browxy_amazon_2/cmpreqs/video/processBrowxyVideo.sh --input outputBlockAdd.mp4 --output outputBlockAdd.gif --fontSize 50 --text "Agregar bloque salida"
+/home/jpconver/workspaces/browxy_amazon_2/cmpreqs/video/processBrowxyVideo.sh --input outputBlockConfigure.mp4 --output outputBlockConfigure.gif --fontSize 50 --text "Configurar bloque salida"
+/home/jpconver/workspaces/browxy_amazon_2/cmpreqs/video/processBrowxyVideo.sh --input outputBlockConfigure.mp4 --output outputBlockConfigure.gif --fontSize 50 --text "Editar bloque salida"
+/home/jpconver/workspaces/browxy_amazon_2/cmpreqs/video/processBrowxyVideo.sh --input flowRunWithInvalidOutput.mp4 --output flowRunWithInvalidOutput.gif --fontSize 50 --text "Correr flujo"
+/home/jpconver/workspaces/browxy_amazon_2/cmpreqs/video/processBrowxyVideo.sh --input flowTestWithInvalidOutput.mp4 --output flowTestWithInvalidOutput.gif --fontSize 50 --text "Verificar flujo"
+/home/jpconver/workspaces/browxy_amazon_2/cmpreqs/video/processBrowxyVideo.sh --input outputBlockConfigureFix.mp4 --output outputBlockConfigureFix.gif --fontSize 50 --text "Editar Flujo"
+/home/jpconver/workspaces/browxy_amazon_2/cmpreqs/video/processBrowxyVideo.sh --input flowSubmission.mp4 --output flowSubmission.gif --fontSize 50 --text "Enviar flujo"
+/home/jpconver/workspaces/browxy_amazon_2/cmpreqs/video/processBrowxyVideo.sh --input flowTestWithInvalidOutput.mp4 --output flowTestWithInvalidOutput.gif --fontSize 50 --text "Verificar Flujo"
+/home/jpconver/workspaces/browxy_amazon_2/cmpreqs/video/processBrowxyVideo.sh --input flowTestWithValidOutput.mp4 --output flowTestWithValidOutput.gif --fontSize 50 --text "Verificar Flujo"
+
