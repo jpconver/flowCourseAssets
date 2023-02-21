542ef66127b1a6032d5e9c73dba8e41e9c44969e
jpconver <jpconver@gmail.com>
2023-02-21 18:18:34 -0300

    remove not needed line

--- scripts/generateVideos.sh
+++ scripts/generateVideos.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
+
 /home/jpconver/workspaces/browxy_amazon_2/cmpreqs/video/processBrowxyVideo.sh --input emptyFlowRun.mp4 --output emptyFlowRun.gif --fontSize 50 --text "Correr flujo vacío"
 /home/jpconver/workspaces/browxy_amazon_2/cmpreqs/video/processBrowxyVideo.sh --input goalView.mp4 --output goalView.gif --fontSize 50 --text "Descripción Desafío"
 /home/jpconver/workspaces/browxy_amazon_2/cmpreqs/video/processBrowxyVideo.sh --input outputBlockAdd.mp4 --output outputBlockAdd.gif --fontSize 50 --text "Agregar bloque salida"
-/home/jpconver/workspaces/browxy_amazon_2/cmpreqs/video/processBrowxyVideo.sh --input outputBlockConfigure.mp4 --output outputBlockConfigure.gif --fontSize 50 --text "Configurar bloque salida"
 /home/jpconver/workspaces/browxy_amazon_2/cmpreqs/video/processBrowxyVideo.sh --input outputBlockConfigure.mp4 --output outputBlockConfigure.gif --fontSize 50 --text "Editar bloque salida"
 /home/jpconver/workspaces/browxy_amazon_2/cmpreqs/video/processBrowxyVideo.sh --input flowRunWithInvalidOutput.mp4 --output flowRunWithInvalidOutput.gif --fontSize 50 --text "Correr flujo"
 /home/jpconver/workspaces/browxy_amazon_2/cmpreqs/video/processBrowxyVideo.sh --input flowTestWithInvalidOutput.mp4 --output flowTestWithInvalidOutput.gif --fontSize 50 --text "Verificar flujo"
